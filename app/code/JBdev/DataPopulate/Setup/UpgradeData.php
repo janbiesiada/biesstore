d9ac6136ba33d09f8a6673c73a070d169051bc2c
Jan Biesiada <jan.biesiada.88@gmail.com>
2017-08-28 21:33:01 +0200

    initial configuration

--- app/code/JBdev/DataPopulate/Setup/UpgradeData.php
+++ app/code/JBdev/DataPopulate/Setup/UpgradeData.php
@@ -1,31 +1,22 @@
 <?php
-/**
- * Copyright © 2013-2017 Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
 
 namespace JBdev\DataPopulate\Setup;
 
-use Magento\Eav\Setup\EavSetupFactory;
-use Magento\Framework\Api\SimpleDataObjectConverter;
 use Magento\Framework\App\Config\Storage\WriterInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\UpgradeDataInterface;
 use Magento\Store\Model\Group as GroupModel;
-use Magento\Store\Model\GroupFactory;
 use Magento\Store\Model\Store;
 use Magento\Store\Model\Store as StoreModel;
 use Magento\Store\Model\StoreFactory;
+use Magento\Store\Model\Website as WebsiteModel;
 
 
 class UpgradeData implements UpgradeDataInterface
 {
     /** @var GroupModel */
     protected $groupModel;
-
-    /** @var GroupFactory */
-    protected $groupFactory;
 
     /** @var StoreModel */
     protected $storeModel;
@@ -36,48 +27,44 @@ class UpgradeData implements UpgradeDataInterface
      * @var WriterInterface
      */
     private $writer;
+    /**
+     * @var WebsiteModel
+     */
+    private $websiteModel;
 
     public function __construct(
         WriterInterface $writer,
+        WebsiteModel $websiteModel,
         GroupModel $groupModel,
-        GroupFactory $groupFactory,
         StoreModel $storeModel,
         StoreFactory $storeFactory
     )
     {
         $this->groupModel = $groupModel;
-        $this->groupFactory = $groupFactory;
         $this->storeModel = $storeModel;
         $this->storeFactory = $storeFactory;
         $this->writer = $writer;
+        $this->websiteModel = $websiteModel;
     }
 
 
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
         $setup->startSetup();
-        if ($this->checkVersion('1.0.1', $context)) {
-            foreach ($this->getConfigData() as $config) {
+        if (version_compare($context->getVersion(), '1.0.1') < 0) {
                 $storeViews = [];
-                $storeViews[] = $this->createStoreWithView('bies','en', 2, true);
-                $storeViews[] = $this->createStoreWithView('bies','pl', 2);
+                $storeViews[] = $this->createStoreWithView('bies','en', 2, 1,true);
+                $storeViews[] = $this->createStoreWithView('bies','pl', 2,2);
 
+            foreach ($this->getConfigData() as $config) {
                 $c = (object)$config;
                 $this->writer->save($c->path, $c->value, $c->scope, $c->scope_id);
 
             }
         }
-
-
         $setup->endSetup();
     }
 
-
-    public function checkVersion($version, ModuleContextInterface $context)
-    {
-        return $context->getVersion()
-            && version_compare($context->getVersion(), $version) < 0;
-    }
 
     public function getConfigData()
     {
@@ -91,19 +78,6 @@ class UpgradeData implements UpgradeDataInterface
             ['scope' => 'stores', 'scope_id' => '2', 'path' => 'general/locale/firstday', 'value' => '1'],
             ['scope' => 'default', 'scope_id' => '0', 'path' => 'general/locale/timezone', 'value' => 'America/Los_Angeles'],
             ['scope' => 'stores', 'scope_id' => '2', 'path' => 'general/locale/weight_unit', 'value' => 'kgs'],
-            ['scope' => 'default', 'scope_id' => '0', 'path' => 'general/region/display_all', 'value' => '1'],
-            ['scope' => 'default', 'scope_id' => '0', 'path' => 'general/region/state_required', 'value' => 'AT,BR,CA,EE,FI,LV,LT,RO,ES,CH,US'],
-            ['scope' => 'default', 'scope_id' => '0', 'path' => 'general/single_store_mode/enabled', 'value' => '0'],
-            ['scope' => 'default', 'scope_id' => '0', 'path' => 'general/store_information/city', 'value' => 'NULL'],
-            ['scope' => 'default', 'scope_id' => '0', 'path' => 'general/store_information/country_id', 'value' => 'NULL'],
-            ['scope' => 'default', 'scope_id' => '0', 'path' => 'general/store_information/hours', 'value' => 'NULL'],
-            ['scope' => 'default', 'scope_id' => '0', 'path' => 'general/store_information/merchant_vat_number', 'value' => 'NULL'],
-            ['scope' => 'default', 'scope_id' => '0', 'path' => 'general/store_information/name', 'value' => 'NULL'],
-            ['scope' => 'default', 'scope_id' => '0', 'path' => 'general/store_information/phone', 'value' => 'NULL'],
-            ['scope' => 'default', 'scope_id' => '0', 'path' => 'general/store_information/postcode', 'value' => 'NULL'],
-            ['scope' => 'default', 'scope_id' => '0', 'path' => 'general/store_information/region_id', 'value' => 'NULL'],
-            ['scope' => 'default', 'scope_id' => '0', 'path' => 'general/store_information/street_line1', 'value' => 'NULL'],
-            ['scope' => 'default', 'scope_id' => '0', 'path' => 'general/store_information/street_line2', 'value' => 'NULL'],
             ['scope' => 'default', 'scope_id' => '0', 'path' => 'web/unsecure/base_url', 'value' => 'http://localhost/bies/'],
             ['scope' => 'stores', 'scope_id' => '1', 'path' => 'web/unsecure/base_url', 'value' => 'http://localhost/bies.en/'],
             ['scope' => 'stores', 'scope_id' => '2', 'path' => 'web/unsecure/base_url', 'value' => 'http://localhost/bies.pl/']
@@ -112,29 +86,32 @@ class UpgradeData implements UpgradeDataInterface
 
     }
 
-    protected function createStoreWithView($code, $storeViewName, $rootCategoryId, $isFirst = false)
+    protected function createStoreWithView($code, $storeViewName, $rootCategoryId, $sortOrder, $isFirst = false)
     {
-        $store = $this->groupModel->load(1);
-        $store
+        $website = $this->websiteModel->load(1);
+        $website->setName(ucfirst($code));
+        $website->save();
+        $storeGroup = $this->groupModel->load(1);
+        $storeGroup
             ->setWebsiteId(1)
             ->setRootCategoryId($rootCategoryId)
+            ->setName(ucfirst($code))
             ->save();
 
-        $storeView = ($isFirst) ? $this->storeModel->load(1) : $this->storeFactory->create();
-        $storeView
+        $store = ($isFirst) ? $this->storeModel->load(1) : $this->storeFactory->create();
+        $store
             ->setName($storeViewName)
             ->setWebsiteId(1)
-            ->setGroupId($store->getGroupId())
+            ->setGroupId($storeGroup->getGroupId())
+            ->setSortOrder($sortOrder)
             ->setIsActive(true);
 
-        if (!$isFirst) {
-            $storeView->setCode("{$code}_{$storeViewName}");
-        }
+        $store->setCode("{$code}_{$storeViewName}");
+        $store->save();
+        $this->writer->save('catalog/category/root_id', $rootCategoryId, 'stores', $storeGroup->getId());
 
-        $storeView->save();
-        $this->writer->save('catalog/category/root_id', $rootCategoryId, 'stores', $store->getId());
 
-        return $storeView;
+        return $store;
     }
 
 }
